27204ff51114ed8ae73bcfbfdadd2df7d4ac31e1
Erwan Darves <erwandarves@Erwans-MacBook-Pro.local>
2024-08-24 02:52:45 +0200

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,38 +122,15 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-tagmanager",
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        id: "GTM-THZBX3D7",
-  
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-  
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-  
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-  
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        routeChangeEventName: "ROUTE_CHANGE",
-        // Defaults to false
-        enableWebVitalsTracking: true,
-        // Defaults to https://www.googletagmanager.com
-        //selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
-        // Defaults to gtm.js
-        //selfHostedPath: "YOUR_SELF_HOSTED_PATH",
+        trackingIds: [
+          'G-YCHMBNGXE2', // Replace with your Google Analytics Measurement ID
+        ],
+        pluginConfig: {
+          head: true,
+        },
       },
-    },
+    }
   ],
 }
